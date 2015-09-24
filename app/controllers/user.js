a6b930e48f649f952548de80687ee9d7cf6f1d51
zhyq0826 <zhyq0826@gmail.com>
2015-09-24 18:05:11 +0800

    fix app controller

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -2,5 +2,12 @@ import Ember from 'ember';
 import form from 'ember-easy-orm/mixins/form'
 
 export default Ember.Controller.extend(form, {
-    modelName:'user'
+    modelName:'user',
+    actions: {
+        callback: function (action, data) {
+            console.log(action);
+            console.log(data);
+            this.toggleProperty('formModalShow');
+        }
+    }
 });
