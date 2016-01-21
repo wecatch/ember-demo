41a90c49bbb235f1c0229ef0c5883a946bd6be31
zhyq0826 <zhyq0826@gmail.com>
2016-01-21 19:46:51 +0800

    add ui test

---
+++ app/components/ui-test.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    blockParams: 'I am blockParams',
+    noBlockParams: 'no blockParams',
+    yesBlock: 'yes block',
+    noBlock: 'no block',
+});
